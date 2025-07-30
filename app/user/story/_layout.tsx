bab33fa17429846e99a8ce8ad3b7dbf484d7b30b
kaitodecode <kaitodecode@gmail.com>
2025-07-30 08:54:41 +0700

    refactor(layouts): hide headers in user screens and reorder tabs
    Consistently hide headers across all user screens by setting headerShown to false. Also reordered the tab screens in the main user layout for better logical grouping.

--- app/user/story/_layout.tsx
+++ app/user/story/_layout.tsx
@@ -2,7 +2,11 @@ import { Stack } from "expo-router";
 
 export default function StoryLayout() {
     return (
-        <Stack>
+        <Stack
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
             <Stack.Screen name="index" />
         </Stack>
     );
